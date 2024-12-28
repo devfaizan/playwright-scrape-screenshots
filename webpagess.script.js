0add778e8c13cd102deb708ef52f50bf2b202b63
devfaizan <mfaizan.zahir@gmail.com>
2024-12-29 00:05:00 +0500

    mobile & web screenshots

--- webpagess.script.js
+++ webpagess.script.js
@@ -1,14 +1,27 @@
 const { firefox, webkit } = require("playwright");
 
-async function screenshots() {
+async function pageAndBrowser() {
     const broswer = await firefox.launch();
     const context = await broswer.newContext();
     const page = await context.newPage();
+    return { broswer, page };
 
+}
+
+async function screenshotWeb() {
+    const { broswer, page } = await pageAndBrowser();
     await page.goto("https://devfaizan.github.io/about.html");
-    await page.setViewportSize({ width: 400, height: 640 });
-    await page.screenshot({ path: `mobile-${new Date().getTime()}.png`, fullPage: true });
+    await page.setViewportSize({ width: 1920, height: 1080 });
+    await page.screenshot({ path: `Web-1080-${new Date().getTime()}.png`, fullPage: true });
+    await broswer.close();
+}
+async function screenshotMobile() {
+    const { broswer, page } = await pageAndBrowser();
+    await page.goto("https://devfaizan.github.io/about.html");
+    await page.setViewportSize({ width: 412, height: 915 });
+    await page.screenshot({ path: `Mobile-${new Date().getTime()}.png`, fullPage: true });
     await broswer.close();
 }
 
-screenshots();
+screenshotWeb();
+screenshotMobile();
